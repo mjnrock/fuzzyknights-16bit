d1086c2b8816ffff5507e46882e14e8fccc9e529
mjnrock <mjnrock@umich.edu>
2018-12-29 17:57:02 -0500

    bug notes

--- public/component/enum/NavigabilityType.js
+++ public/component/enum/NavigabilityType.js
@@ -10,7 +10,7 @@ const NavigabilityType = {
 NavigabilityType.GetConstraint = function(value) {
 	const Speed = {
 		NONE:			0,
-		WATER:			0,
+		WATER:			0.25,
 		SAND:			0.33,
 		GRASS:			1,
 		ROCK:			0.5,
